568cf6e4b0fba5455c3c9d113892ef59293f59f5
Michele Balestrini <michele.balestrini@gmail.com>
2013-05-13 19:13:13 +0200

    Feature: Inserimento e modifica specie pignatti
    Controlli d'input sulla specie

---
+++ app/controllers/admin/pignatti_controller.rb
@@ -0,0 +1,92 @@
+class Admin::PignattiController < ApplicationController
+  def index
+    @pignatti = Specie.find(:all)
+  end
+
+  def new
+    @new_specie = Specie.new
+    @euflora = Euflora.find(:all,:order => "descrizione")
+    @pignatti = Specie.find(:all)
+    #apro una finestra di input
+    render :update do |page|
+      page.hide "display_input_errors"
+      page.show "new_specie"
+      page.replace_html "pignatti_list", :partial => "specie_list", :object => @pignatti
+      page.replace_html 'new_specie', :partial => 'new_specie_form', :object => [@new_specie,@euflora]
+    end
+  end
+
+  def close_new_specie
+    #chiudo gli errori sull'input e il nuovo input
+    render :update do |page|
+      page.hide "new_specie"
+      page.hide "display_input_errors"
+    end
+  end
+
+  def save_specie
+    #compilo i campi del nuovo plot
+    @new_specie = Specie.new(params[:specie])
+
+    #se riesco a salvare il plot passando tutte le restrizioni
+    if @new_specie.save
+      #carico nuovamente i plot
+      @pignatti = Specie.find(:all)
+      #aggiorno la tabella dei plot e chiudo la maschera di input
+      @message = "Nuova specie aggiunta."
+      render :update do |page|
+        page.hide "display_input_errors"
+        page.hide "new_specie"
+        page.replace_html "pignatti_list", :partial => "specie_list", :object => [@pignatti,@message]
+      end
+    else
+      #mostro gli errori sull'input
+      render :update do |page|
+        page.show "display_input_errors"
+        page.replace_html "display_input_errors", :partial => "input_errors", :object => @new_specie
+      end
+    end
+
+  end
+
+  def edit
+    @id = params[:id]
+    @i = params[:i]
+    #ricarico le specie
+    @pignatti = Specie.find(:all)
+    @euflora = Euflora.find(:all, :order => "descrizione")
+    render :update do |page|
+      page.hide "new_specie"
+      page.hide "display_input_errors"
+      page.replace_html "pignatti_list", :partial => "edit_specie", :object => [:@id,@i,@pignatti,@euflora]
+    end
+  end
+
+  def close_edit
+    #ricarico i plot
+    @pignatti = Specie.find(:all)
+    render :update do |page|
+      page.hide "display_input_errors"
+      page.replace_html "pignatti_list", :partial => "specie_list", :object => @pignatti
+    end
+  end
+
+  def save_edit
+    @new_specie = Specie.find(params[:id])
+    if @new_specie.update_specie(params[:descrizione],params[:euflora_id])
+      @pignatti = Specie.find(:all)
+      @message = "Modifica salvata."
+      render :update do |page|
+        page.hide "display_input_errors"
+        page.replace_html "pignatti_list", :partial => "specie_list", :object => [@pignatti,@message]
+      end
+    else
+      @pignatti = Specie.find(:all)
+      render :update do |page|
+        page.show "display_input_errors"
+        page.replace_html "display_input_errors", :partial => "input_errors", :object => @new_specie
+      end
+    end
+  end
+
+end
--- app/models/specie.rb
+++ app/models/specie.rb
@@ -1,3 +1,27 @@
 class Specie < ActiveRecord::Base
   set_table_name "specie"
+
+  validates_presence_of :descrizione, :message => "non può essere vuoto."
+  validates_length_of :descrizione, :maximum => 100 , :message => "massimo 100 caratteri."
+  validate :no_dup
+
+  def no_dup
+    #carico tutti i plot non eliminati
+    specie_list = Specie.find(:all)
+    unless specie_list.blank?
+      #per ogni plot
+      specie_list.each do |spec|
+        if spec.descrizione == descrizione && spec.id != self.id
+          #segnalo l'errore
+          errors.add(:descrizione, " già presente tra le specie.")
+        end
+      end
+    end
+  end
+
+  def update_specie(desc,euflora)
+    self.descrizione = desc
+    self.euflora_id = euflora
+    return true if self.save
+  end
 end
