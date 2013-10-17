1ca4a14ab783d6736845d6bf496fc8a80de2a462
Michele Balestrini <michele.balestrini@gmail.com>
2013-10-17 11:59:46 +0200

    Shadow Grafic

--- app/controllers/admin/file_manager_controller.rb
+++ app/controllers/admin/file_manager_controller.rb
@@ -1,6 +1,9 @@
 class Admin::FileManagerController < ApplicationController
 
   def index
+  end
+
+  def result
     @import_file = ImportFile.find(:all,:conditions => ["deleted = false"], :order => "file_name")
   end
 
